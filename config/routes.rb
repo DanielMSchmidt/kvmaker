43226957874bb23e367a197703e2152f37b0b17e
Daniel Schmidt <dschmidt@weluse.de>
2012-12-06 10:42:20 +0100

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ KVmaker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'kvs#index'
 
   # See how all your routes lay out with "rake routes"
 
